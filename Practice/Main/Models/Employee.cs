baafd0b81f8c943a9e04fe1913807febda691124
Ali200119 <aliit@code.edu.az>
2023-01-09 00:46:13 +0400

    feat: Operator overloading

---
+++ Practice/Main/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Main.Models
+{
+	public class Employee
+	{
+		public int Id { get; set; }
+		public int Age { get; set; }
+		public string Name { get; set; }
+
+		public static bool operator >(Employee emp1, Employee emp2)
+		{
+			return emp1.Age > emp2.Age;
+		}
+
+        public static bool operator <(Employee emp1, Employee emp2)
+        {
+            return emp1.Age < emp2.Age;
+        }
+    }
+}
---
+++ Practice/Main/Program.cs
@@ -0,0 +1,24 @@
+﻿using Main.Models;
+
+#region Operator overloading
+
+//Employee employee1 = new Employee
+//{
+//    Id = 1,
+//    Name = "Ali",
+//    Age = 30
+//};
+
+//Employee employee2 = new Employee
+//{
+//    Id = 2,
+//    Name = "Cavid",
+//    Age = 29
+//};
+
+//var result = employee1 > employee2;
+
+//Console.WriteLine(result);
+
+#endregion
+
