793d401c34e1af3c3752dbd0f41dbe41d82730a0
Ali200119 <aliit@code.edu.az>
2023-01-09 05:22:10 +0400

    Practice

--- Practice/Main/Program.cs
+++ Practice/Main/Program.cs
@@ -85,17 +85,64 @@ using Main.Models;
 //datas.Add(30);
 //datas.Add(19);
 
-////datas.Sort();
-////datas.Reverse();
+//datas.Sort();
+//datas.Reverse();
 
-////datas.Sort();
+//datas.Sort();
 
-////Console.WriteLine(datas[0]);
+//Console.WriteLine(datas[0]);
+
+//datas.Remove(30);
+
+//datas.RemoveAt(2);
+
+//datas.Clear();
+
+//datas.RemoveAll(m=>m>20);
+
+//bool res = datas.Contains(19);
+
+//Console.WriteLine(res);
 
 //foreach (var item in datas)
 //{
 //    Console.WriteLine(item);
 //}
+
+//List<string> names = new List<string>() { "Cavid", "Cavid", "Aqshin", "Mirze", "Elcan" };
+
+//Console.WriteLine(names.Count());
+
+//foreach (var item in names)
+//{
+//    if (item == "Cavid")
+//    {
+//        Console.WriteLine(item);
+//    }
+//}
+
+//for (int i = 0; i < names.Count(); i++)
+//{
+//    Console.WriteLine(names[i]);
+//}
+
+//string? result = names.Find(n => n == "Cavid");
+//Console.WriteLine(result);
+
+//int result = names.FindAll(n => n == "Cavid").Count();
+
+//Console.WriteLine(result);
+
+//foreach (var item in result)
+//{
+//    Console.WriteLine(item);
+//}
+
+//List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+
+//Console.WriteLine(numbers.Sum(n => n));
+
+//Console.WriteLine(numbers.FindAll(n => n%2 == 0).Sum(n => n));
 
 #endregion
 
